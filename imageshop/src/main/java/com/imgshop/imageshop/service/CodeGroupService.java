bcb227787184877e556a268ab09d3483d1877e7d
kimsolee <ahhasolee@naver.com>
2022-01-16 11:49:34 +0900

    Revert "코드그룹 register기능 구현"
    This reverts commit 595c3ff100ea93c459ca63c702d06d522300623c.

--- imageshop/src/main/java/com/imgshop/imageshop/service/CodeGroupService.java
+++
@@ -1,8 +0,0 @@
-package com.imgshop.imageshop.service;
-
-import com.imgshop.imageshop.domain.CodeGroup;
-
-public interface CodeGroupService {
-
-	public int codeGroupregister(CodeGroup codeClass) throws Exception;
-}
